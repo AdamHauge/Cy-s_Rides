2ae0f25a409a2a80b585c65c27747b0adbf97213
Adam Hauge <ahauge@iastate.edu>
2017-09-09 20:16:04 -0500

    Added menu button handler.

--- CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
+++ CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
@@ -2,6 +2,10 @@ package cysrides.cysrides;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_item1:
+                Toast.makeText(getApplicationContext(), "My Profile", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_item2:
+                Toast.makeText(getApplicationContext(), "Offer Rides", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_item3:
+                Toast.makeText(getApplicationContext(), "Ride Requests", Toast.LENGTH_LONG).show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
