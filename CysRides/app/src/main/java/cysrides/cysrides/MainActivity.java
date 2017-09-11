18f81a58a6e8df338e2bb7097c6b4eae4b1ab244
Adam Hauge <ahauge@iastate.edu>
2017-09-11 18:50:54 -0500

    Fixed action bar issue, Linked profile and registration pages to home page

--- CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
+++ CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId()) {
             case R.id.register:
                 Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+                i = new Intent(MainActivity.this, CreateProfile.class);
+                startActivity(i);
                 break;
                 case R.id.login:
                 Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
@@ -38,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.profile:
                 Toast.makeText(getApplicationContext(), "My Profile", Toast.LENGTH_SHORT).show();
+                i = new Intent(MainActivity.this, ViewProfile.class);
+                startActivity(i);
                 break;
             case R.id.contacts:
                 Toast.makeText(getApplicationContext(), "Contacts", Toast.LENGTH_SHORT).show();
