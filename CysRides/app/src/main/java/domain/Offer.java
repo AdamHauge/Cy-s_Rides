84210815ed720502ceaa52cabf0b675eab840bbe
ZachKnoll <zgknoll@iastate.edu>
2017-09-11 14:45:42 -0500

    Created Offer object and user type enum class

---
+++ CysRides/app/src/main/java/domain/Offer.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Offer {
+
+    private UserType userType;
+    private double cost;
+    private String user;
+    private String destination;
+    private String description;
+
+    public Offer() {
+
+    }
+}
