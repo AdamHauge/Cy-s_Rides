efc559c83438368075baa198fba331ecb4248d0d
Adam Hauge <ahauge@iastate.edu>
2017-09-12 21:09:45 -0500

    Added some homepage functionality as well as other minor changes

--- CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
+++ CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
@@ -6,15 +6,39 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayList list = new ArrayList();
+    private ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO what is going to get put on our main page???
+        listView = (ListView) findViewById(R.id.main_list);
+
+        for (int i = 0; i < 20; i++) { //TODO the number needs to match the amount of contacts
+            list.add("Item #" + (i + 1)); //TODO this needs to be changed to match the contact names
+        }
+
+        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_2, android.R.id.text1, list);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), list.get(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
