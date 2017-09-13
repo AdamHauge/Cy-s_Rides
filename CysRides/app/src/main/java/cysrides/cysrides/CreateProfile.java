1afa9c960dc413cc6c5ae7110eb88ab6ef572360
cathens <cathens@iastate.edu>
2017-09-13 00:07:38 -0500

    Worked on CreateProfile page.

--- CysRides/app/src/main/java/cysrides/cysrides/CreateProfile.java
+++ CysRides/app/src/main/java/cysrides/cysrides/CreateProfile.java
@@ -2,6 +2,11 @@ package cysrides.cysrides;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.CompoundButton;
+
+import domain.UserInfo;
 
 public class CreateProfile extends AppCompatActivity {
 
@@ -10,4 +15,23 @@ public class CreateProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
     }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.passengerRadioButton:
+                if (checked) {
+                    break;
+                }
+            case R.id.driverRadioButton:
+                if (checked)
+                    break;
+        }
+    }
+
+
+
 }
