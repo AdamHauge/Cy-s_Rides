d97472e91a8f5de42be332050665a7acddabaab8
ahauge <ahauge@iastate.edu>
2017-09-17 15:13:27 -0500

    Menu has been changed to side drawer

--- CysRides/app/src/main/java/cysrides/cysrides/Contacts.java
+++ CysRides/app/src/main/java/cysrides/cysrides/Contacts.java
@@ -1,7 +1,15 @@
 package cysrides.cysrides;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,7 +19,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Contacts extends AppCompatActivity {
+public class Contacts extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView listView;
     private ArrayList list = new ArrayList();
@@ -21,6 +29,17 @@ public class Contacts extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         listView = (ListView)findViewById(R.id.contacts_list);
 
@@ -36,5 +55,94 @@ public class Contacts extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), list.get(position).toString(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent i;
+
+        switch(id)
+        {
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "My Profile", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, ViewProfile.class);
+                startActivity(i);
+                break;
+            case R.id.requests:
+                Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, RideRequests.class);
+                startActivity(i);
+                break;
+            case R.id.register:
+                Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, CreateProfile.class);
+                startActivity(i);
+                break;
+            case R.id.login:
+                Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, ViewProfile.class);
+                startActivity(i);
+                break;
+            case R.id.contacts:
+                Toast.makeText(getApplicationContext(), "Contacts", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, Contacts.class);
+                startActivity(i);
+                break;
+            case R.id.createProfile:
+                Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, CreateProfile.class);
+                startActivity(i);
+                break;
+            case R.id.createOffer:Toast.makeText(getApplicationContext(), "Create Offer", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, CreateOffer.class);
+                startActivity(i);
+                break;
+            case R.id.createRequest:
+                Toast.makeText(getApplicationContext(), "Create Request", Toast.LENGTH_SHORT).show();
+                i = new Intent(Contacts.this, CreateRequest.class);
+                startActivity(i);
+                break;
+            default:
+                break;
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
+++ CysRides/app/src/main/java/cysrides/cysrides/MainActivity.java
@@ -1,69 +1,98 @@
 package cysrides.cysrides;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity {
-
-    private ListView listView;
-    private ArrayList list = new ArrayList();
-    private ArrayAdapter adapter;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //TODO what is going to get put on our main page???
-        listView = (ListView) findViewById(R.id.main_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        for (int i = 0; i < 5; i++) { //TODO the number needs to match the amount of contacts
-            list.add("Ride Offer"); //TODO this needs to be changed to match the contact names
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
-
-        adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_2, android.R.id.text1, list);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), list.get(position).toString(), Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
         Intent i;
-        switch(item.getItemId()) {
+
+        switch(id)
+        {
+            case R.id.profile:
+                Toast.makeText(getApplicationContext(), "My Profile", Toast.LENGTH_SHORT).show();
+                i = new Intent(MainActivity.this, ViewProfile.class);
+                startActivity(i);
+                break;
+            case R.id.requests:
+                Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
+                i = new Intent(MainActivity.this, RideRequests.class);
+                startActivity(i);
+                break;
             case R.id.register:
                 Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
                 i = new Intent(MainActivity.this, CreateProfile.class);
                 startActivity(i);
                 break;
-                case R.id.login:
+            case R.id.login:
                 Toast.makeText(getApplicationContext(), "Login", Toast.LENGTH_SHORT).show();
-                i = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(i);
-                break;
-            case R.id.profile:
-                Toast.makeText(getApplicationContext(), "My Profile", Toast.LENGTH_SHORT).show();
                 i = new Intent(MainActivity.this, ViewProfile.class);
                 startActivity(i);
                 break;
@@ -72,28 +101,26 @@ public class MainActivity extends AppCompatActivity {
                 i = new Intent(MainActivity.this, Contacts.class);
                 startActivity(i);
                 break;
-            case R.id.requests:
-                Toast.makeText(getApplicationContext(), "Ride Requests", Toast.LENGTH_SHORT).show();
-                i = new Intent(MainActivity.this, RideRequests.class);
-                startActivity(i);
-                break;
             case R.id.createProfile:
+                Toast.makeText(getApplicationContext(), "Register", Toast.LENGTH_SHORT).show();
                 i = new Intent(MainActivity.this, CreateProfile.class);
                 startActivity(i);
                 break;
-            case R.id.createOffer:
-                Toast.makeText(getApplicationContext(), "Create New Offer", Toast.LENGTH_SHORT).show();
+            case R.id.createOffer:Toast.makeText(getApplicationContext(), "Create Offer", Toast.LENGTH_SHORT).show();
                 i = new Intent(MainActivity.this, CreateOffer.class);
                 startActivity(i);
                 break;
             case R.id.createRequest:
-                Toast.makeText(getApplicationContext(), "Create New Request", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Create Request", Toast.LENGTH_SHORT).show();
                 i = new Intent(MainActivity.this, CreateRequest.class);
                 startActivity(i);
                 break;
             default:
-                return super.onOptionsItemSelected(item);
+                break;
         }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 }
