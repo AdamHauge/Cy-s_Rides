ca191290db61c5fc47dfabd142b4c51b31972c16
cathens <cathens@iastate.edu>
2017-10-08 19:18:22 -0500

    Worked on creating the user object and sending to database

---
+++ CysRides/app/src/main/java/volley/EmailVolley.java
@@ -0,0 +1,8 @@
+package volley;
+
+import android.content.Context;
+import android.view.View;
+
+public interface EmailVolley {
+    void sendEmail(Context context, View view);
+}
--- CysRides/app/src/main/java/volley/UserVolley.java
+++ CysRides/app/src/main/java/volley/UserVolley.java
@@ -1,10 +1,11 @@
 package volley;
 
 import android.content.Context;
+import android.view.View;
 
 import domain.UserInfo;
 
 public interface UserVolley {
-    void createUser(Context context, UserInfo user);
-    void getUser(Context context, UserInfo user);
+    void createUser(Context context, View view, UserInfo user);
+    void getUser(Context context, View view, UserInfo user);
 }
