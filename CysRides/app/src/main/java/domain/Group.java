8ae788b18a8aa8053b2952e6b620d81d4a7a5bdb
rcerveny <rcerveny@iastate.edu>
2017-10-17 23:13:08 -0500

    Created Group and made database tables

---
+++ CysRides/app/src/main/java/domain/Group.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Ryan on 10/17/2017.
+ */
+
+public class Group {
+    ArrayList<UserInfo> groupMembers = new ArrayList<>();
+
+    public Group(){
+
+    }
+
+
+}
