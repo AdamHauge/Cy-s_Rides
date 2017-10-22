dacd33b4ec4ce1e845974c506fa5c852efbdbcb1
Adam Hauge <ahauge@iastate.edu>
2017-10-22 15:56:25 -0500

    test

--- CysRides/app/src/main/java/cysrides/cysrides/ViewOffer.java
+++ CysRides/app/src/main/java/cysrides/cysrides/ViewOffer.java
@@ -11,19 +11,5 @@ public class ViewOffer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_offer);
 
-        EditText temp = (EditText)findViewById(R.id.UserName);
-        temp.setText(getIntent().getExtras().getString("UserName"));
-
-        temp = (EditText)findViewById(R.id.Destination);
-        temp.setText(getIntent().getExtras().getString("Dest"));
-
-        temp = (EditText)findViewById(R.id.Cost);
-        temp.setText("$" + getIntent().getStringExtra("Cost"));
-
-        temp = (EditText)findViewById(R.id.LeaveDate);
-        temp.setText(getIntent().getStringExtra("Date"));
-
-        temp = (EditText)findViewById(R.id.Description);
-        temp.setText(getIntent().getStringExtra("Description"));
     }
 }
