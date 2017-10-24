67e448959044d2ff742cf827414a4e3fb1e9d08d
rcerveny <rcerveny@iastate.edu>
2017-10-24 18:53:13 -0500

    can now add specific riders to the group thru postman. still not in client

--- CysRides/app/phpFiles/addRider.php
+++ CysRides/app/phpFiles/addRider.php
@@ -1,13 +1,10 @@
 <?php
 
-$driver = $_POST["driver"];
-// $rider1 = $_POST["rider1"];
-// $rider2 = $_POST["rider2"];
-// $rider3 = $_POST["rider3"];
-// $rider4 = $_POST["rider4"];
-// $rider5 = $_POST["rider5"];
-// $rider6 = $_POST["rider6"];
-// $rider7 = $_POST["rider7"];
+
+$group_id = $_POST["id"];
+$rider = $_POST["rider"];
+$rider_num = $_POST["rider_num"];
+
 
 $host="mysql.cs.iastate.edu";
 $port=3306;
@@ -15,10 +12,12 @@ $socket="";
 $username = 'dbu309sab5';
 $password = 'VRCc@3V2';
 $dbname = 'db309sab5';
+$sel_column = 'RIDER_' .$rider_num;
 
 $con = new mysqli($host, $username, $password, $dbname, $port, $socket) or die('Could not connect to database server'.mysqli_connect_error);
 
-$sql = "INSERT INTO GROUP_TABLE (DRIVER/*, RIDER_1, RIDER_2, RIDER_3, RIDER_4, RIDER_5, RIDER_6, RIDER_7*/) VALUES ('".$driver."');";
+$sql =  "UPDATE GROUP_TABLE SET " .$sel_column. " = '" .$rider. "' WHERE ID = " .$group_id. ";";
+//"INSERT INTO GROUP_TABLE (DRIVER/*, RIDER_1, RIDER_2, RIDER_3, RIDER_4, RIDER_5, RIDER_6, RIDER_7*/) VALUES ('".$driver."');";
   //end of values statement
   //,'".$rider1."','".$rider2."','".$rider3."','".$rider4."','".$rider5."','".$rider6."','".$rider7."'
 if(mysqli_query($con,$sql)) {
