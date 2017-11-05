da65f4cb50fdd89e83eb4b9a201275e8cfc0986d
ZachKnoll <zgknoll@iastate.edu>
2017-11-05 12:32:05 -0600

    Started hiding navigation options based off usertype

--- CysRides/app/src/main/java/service/NavigationService.java
+++ CysRides/app/src/main/java/service/NavigationService.java
@@ -3,9 +3,13 @@ package service;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.view.Menu;
 import android.view.MenuItem;
+
+import domain.UserInfo;
 
 public interface NavigationService {
     Intent getNavigationIntent(@NonNull MenuItem item, Context c, Intent intent);
     boolean checkInternetConnection(Context c);
+    void hideMenuItems(Menu menu, UserInfo userInfo);
 }
