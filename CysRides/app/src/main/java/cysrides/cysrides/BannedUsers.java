077c11b3cd2c955b53826fdb5331bdbd773b7333
Zach Knoll <zgknoll@iastate.edu>
2017-11-05 21:42:56 -0600

    commit

--- CysRides/app/src/main/java/cysrides/cysrides/BannedUsers.java
+++ CysRides/app/src/main/java/cysrides/cysrides/BannedUsers.java
@@ -6,13 +6,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -27,33 +30,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import domain.Ban;
-import domain.Offer;
 import service.NavigationService;
 import service.NavigationServiceImpl;
 import service.UserIntentService;
 import service.UserIntentServiceImpl;
-import volley.BanVolley;
 import volley.BanVolleyImpl;
-import volley.OfferVolleyImpl;
 
 public class BannedUsers extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private UserIntentService userIntentService = new UserIntentServiceImpl();
     private NavigationService navigationService = new NavigationServiceImpl();
 
+    private Intent i;
+    private SwipeRefreshLayout refresh;
     private ArrayAdapter<String> adapter;
     private List<Ban> bans = new ArrayList<>();
-    FragmentManager fragmentManager = this.getSupportFragmentManager();
-    private Intent i;
+    private FragmentManager fragmentManager = this.getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride_offers);
+        setContentView(R.layout.activity_banned_users);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.ride_offers_activity);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.banned_users_activity);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -62,24 +63,35 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        Menu menu = navigationView.getMenu();
+        navigationService.hideMenuItems(menu, userIntentService.getUserFromIntent(this.getIntent()));
+
+        refresh = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        refresh.setColorSchemeColors(ContextCompat.getColor(this.getApplicationContext(),
+                R.color.colorGold), ContextCompat.getColor(this.getApplicationContext(), R.color.colorCardinal));
+        refresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getBansList();
+            }
+        });
         getBansList();
-//TODO
-        ListView listView = (ListView)findViewById(R.id.ride_offers_list);
-//        adapter = new ArrayAdapter<>(BannedUsers.this, android.R.layout.simple_list_item_1, destinations);
-//        listView.setAdapter(adapter);
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
-//                ViewOffer viewOffer = new ViewOffer();
-//                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-//
-//                viewOffer.setData(offers.get(position));
-//
-//                fragmentTransaction.replace(R.id.ride_offers_activity, viewOffer);
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
-//            }
-//        });
+
+        ListView listView = (ListView)findViewById(R.id.banned_users_list);
+        adapter = new ArrayAdapter<>(BannedUsers.this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                ViewBannedUser viewBannedUser = new ViewBannedUser();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+
+                viewBannedUser.setData(bans.get(position));
+                fragmentTransaction.replace(R.id.banned_users_activity, viewBannedUser);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
 
         if(navigationService.checkInternetConnection(getApplicationContext())) {
             connectionPopUp();
@@ -100,6 +112,19 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
 
                 adapter.clear();
                 adapter.notifyDataSetChanged();
+
+                if(refresh.isRefreshing()) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            refresh.setRefreshing(false);
+                            adapter.notifyDataSetChanged();
+                        }
+                    }, 1000);
+                }
+                else {
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
         volley.execute();
@@ -107,7 +132,7 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.ride_offers_activity);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.banned_users_activity);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
@@ -117,6 +142,7 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
         else {
             finish();
             i = new Intent(BannedUsers.this, MainActivity.class);
+            i = userIntentService.createIntent(BannedUsers.this, MainActivity.class, userIntentService.getUserFromIntent(this.getIntent()));
             startActivity(i);
         }
     }
@@ -158,7 +184,6 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
             alert.setMessage("Do you really want to logout?");
             alert.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
-                    SaveSharedPreference.clearUsernamePassword(BannedUsers.this);
                     startActivity(i);
                 }});
             alert.setNegativeButton(android.R.string.no, null);
@@ -183,7 +208,7 @@ public class BannedUsers extends AppCompatActivity implements NavigationView.OnN
     }
 
     public void connectionPopUp() {
-        Snackbar snackbar = Snackbar.make(findViewById(R.id.ride_offers_activity),
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.banned_users_activity),
                 "Cy's Rides Requires\nInternet Connection", Snackbar.LENGTH_INDEFINITE);
 
         snackbar.setAction("Connect WIFI", new View.OnClickListener() {
