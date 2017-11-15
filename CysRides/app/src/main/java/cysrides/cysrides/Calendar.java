cadca1f34c8492ad25e577072465948d323f81d9
cathens <cathens@iastate.edu>
2017-11-14 22:37:52 -0600

    Started implementation of Calendar. Updated menu bar, side bar, etc. UI for profile and calendar.

--- CysRides/app/src/main/java/cysrides/cysrides/Calendar.java
+++ CysRides/app/src/main/java/cysrides/cysrides/Calendar.java
@@ -3,7 +3,6 @@ package cysrides.cysrides;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -11,14 +10,11 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.RatingBar;
-import android.widget.TextView;
 
-import domain.UserInfo;
 import service.ActivityService;
 import service.ActivityServiceImpl;
 import service.NavigationService;
@@ -26,50 +22,23 @@ import service.NavigationServiceImpl;
 import service.UserIntentService;
 import service.UserIntentServiceImpl;
 
-public class ViewProfile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+public class Calendar extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-    private NavigationService navigationService = new NavigationServiceImpl();
     private UserIntentService userIntentService = new UserIntentServiceImpl();
+    private NavigationService navigationService = new NavigationServiceImpl();
     private ActivityService activityService = new ActivityServiceImpl();
-
-    private UserInfo user;
-    private TextView netIDView;
-    private TextView firstNameView;
-    private TextView lastNameView;
-    private TextView descriptionView;
-    private TextView venmoView;
-    private RatingBar userRatingBar;
 
     private Intent i;
 
-    /*
-    When this activity is created, it initializes all the UI components to the values of the UserInfo
-    object.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
 
-        setContentView(R.layout.activity_view_profile);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        netIDView = (TextView) findViewById(R.id.netIDView);
-        firstNameView = (TextView) findViewById(R.id.firstNameView);
-        lastNameView = (TextView) findViewById(R.id.lastNameView);
-        descriptionView = (TextView) findViewById(R.id.descriptionView);
-        venmoView = (TextView) findViewById(R.id.venmoView);
-        userRatingBar = (RatingBar) findViewById((R.id.userRatingBar));
+//        setSupportActionBar(toolbar);
 
-        user = userIntentService.getUserFromIntent(this.getIntent());
-
-        netIDView.setText(user.getNetID().split("@iastate.edu")[0]);
-        firstNameView.setText(user.getFirstName());
-        lastNameView.setText(user.getLastName());
-        descriptionView.setText(user.getProfileDescription());
-        venmoView.append(user.getVenmoName());
-        userRatingBar.setRating(user.getUserRating());
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.my_profile);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_calendar);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -80,21 +49,23 @@ public class ViewProfile extends AppCompatActivity implements NavigationView.OnN
 
         Menu menu = navigationView.getMenu();
         navigationService.hideMenuItems(menu, userIntentService.getUserFromIntent(this.getIntent()));
-
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.my_profile);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_calendar);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
             finish();
-            i = userIntentService.createIntent(ViewProfile.this, MainActivity.class, userIntentService.getUserFromIntent(this.getIntent()));
+            i = userIntentService.createIntent(Calendar.this, MainActivity.class, userIntentService.getUserFromIntent(this.getIntent()));
             startActivity(i);
         }
     }
 
+    /*
+     * method that initializes menu
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -111,26 +82,34 @@ public class ViewProfile extends AppCompatActivity implements NavigationView.OnN
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.my_profile) {
-            i = userIntentService.createIntent(ViewProfile.this, ViewProfile.class, userIntentService.getUserFromIntent(this.getIntent()));
-            i.putExtra("caller", "View Profile");
+            i = userIntentService.createIntent(Calendar.this, ViewProfile.class, userIntentService.getUserFromIntent(this.getIntent()));
+            i.putExtra("caller", "Calendar");
             startActivity(i);
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+
+    /*
+         * method to handle user's menu selection
+         *
+         * Param: selected menu item
+         */
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        i = navigationService.getNavigationIntent(item, ViewProfile.this, this.getIntent());
+        i = navigationService.getNavigationIntent(item, Calendar.this, this.getIntent());
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.my_profile);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.activity_calendar);
         drawer.closeDrawer(GravityCompat.START);
         if(R.id.logout == id) {
             AlertDialog.Builder alert = navigationService.logOutButton(this.getApplicationContext());
             alert.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                 public void onClick(DialogInterface dialog, int whichButton) {
-                    SaveSharedPreference.clearUsernamePassword(ViewProfile.this);
+                    SaveSharedPreference.clearUsernamePassword(Calendar.this);
                     startActivity(i);
                 }});
             alert.show();
@@ -148,7 +127,7 @@ public class ViewProfile extends AppCompatActivity implements NavigationView.OnN
     }
 
     public void connectionPopUp() {
-        Snackbar snackbar = activityService.setupConnection(this.getApplicationContext(), findViewById(R.id.my_profile));
+        Snackbar snackbar = activityService.setupConnection(this.getApplicationContext(), findViewById(R.id.activity_calendar));
         snackbar.show();
     }
 }
