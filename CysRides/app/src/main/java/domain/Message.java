5d237035620d03a61f96336a5b5667debe1bfd0c
rcerveny <rcerveny@iastate.edu>
2017-12-03 16:50:09 -0600

    added message constructor for creating messages to send to database

--- CysRides/app/src/main/java/domain/Message.java
+++ CysRides/app/src/main/java/domain/Message.java
@@ -13,7 +13,7 @@ public class Message {
   private int id;
   private Date timeSent;
 
-
+  //constructor for pulling messages from the database
   public Message(int id, int groupID, String sender, String message, Date timeSent){
     this.id = id;
     this.message = message;
@@ -21,6 +21,14 @@ public class Message {
     this.groupID = groupID;
     this.timeSent = timeSent;
   }
+  //constructor for creating a message to send to database(doesnt include message id or timeSend
+  //since they will be created in the database
+  public Message(int groupID, String sender, String message){
+      this.groupID = groupID;
+      this.sender = sender;
+      this.message = message;
+  }
+
 
   public String getMessage(){
     return message;
