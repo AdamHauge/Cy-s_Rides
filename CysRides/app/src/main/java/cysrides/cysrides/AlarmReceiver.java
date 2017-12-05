ec8f34a3bc6a7fe394586b1bba50534f32c8fbe2
cathens <cathens@iastate.edu>
2017-12-04 20:06:56 -0600

    Notification is sent/received at time for ride. Need to add intent to notification.

--- CysRides/app/src/main/java/cysrides/cysrides/AlarmReceiver.java
+++ CysRides/app/src/main/java/cysrides/cysrides/AlarmReceiver.java
@@ -1,14 +1,46 @@
 package cysrides.cysrides;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
 import android.widget.Toast;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "Alarm received!", Toast.LENGTH_LONG).show();
+        showNotification(context);
+
+    }
+
+    public void showNotification(Context context) {
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder b = new NotificationCompat.Builder(context);
+
+        b.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_calendar)
+                .setTicker("Cy's Rides")
+                .setContentTitle("Upcoming trip with Cy's Rides")
+                .setContentText("")
+                .setDefaults(Notification.DEFAULT_LIGHTS| Notification.DEFAULT_SOUND)
+                .setContentIntent(contentIntent)
+                .setContentInfo("Info");
+
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, b.build());
     }
 }
